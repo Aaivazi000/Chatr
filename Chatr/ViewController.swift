bf91b271a5e1fc6a06bba8633613329aa685e9c5
Andriana Aivazians <Aaivazi000@citymail.cuny.edu>
2018-10-28 16:32:49 -0400

    Initial Commit

---
+++ Chatr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chatr
+//
+//  Created by Andriana Aivazians on 10/28/18.
+//  Copyright © 2018 Andriana Aivazians. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
